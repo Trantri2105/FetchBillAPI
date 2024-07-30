ac722d64d77e54f2771926b4938ad1c84dbed963
Nguyen Tran Tri <tringuyenhn03@gmail.com>
2024-07-30 15:39:56 +0700

    Add content type to json in response header

--- transport/transport.go
+++ transport/transport.go
@@ -23,6 +23,7 @@ func decodeRequest(_ context.Context, r *http.Request) (interface{}, error) {
 }
 
 func encodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	return json.NewEncoder(w).Encode(response)
 }
 
